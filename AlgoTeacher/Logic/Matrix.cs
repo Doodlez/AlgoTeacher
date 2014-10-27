7292fcfdc570863e47e4a6ac83a44a32678d38e6
Ermakov Vladislav <vlad-093@yandex.ru>
2014-10-27 11:06:29 +0400

    AlgoTeacher: добавлен класс для перемножения матриц (логика). Редактированы события (QuestEventArgs и FillEventArgs).

--- AlgoTeacher/Logic/Matrix.cs
+++ AlgoTeacher/Logic/Matrix.cs
@@ -10,6 +10,8 @@ namespace AlgoTeacher
 {
     public class Matrix
     {
+       
+
         public int Rows
         {
             get;
@@ -42,73 +44,6 @@ namespace AlgoTeacher
                     Values[i][j] = 0;
                 }
             }
-        }
-
-        public static Matrix MatrixMult(Matrix matrix1, Matrix matrix2)
-        {
-            if (matrix1.Columns != matrix2.Rows)
-                return null;
-
-            var coords = GetRandomCoords(matrix1.Rows, matrix2.Columns);
-
-            var resultMatrix = new Matrix(matrix1.Rows, matrix2.Columns);
-            for (var i = 0; i < matrix1.Rows; i++)
-            {
-                for (var j = 0; j < matrix2.Columns; j++)
-                {
-                    for (var k = 0; k < matrix1.Columns; k++)
-                    {
-                        resultMatrix.Values[i][j] += matrix1.Values[i][k] * matrix2.Values[k][j];
-                        if (Coordinate.DoesCoordinateExist(new Coordinate(i, j), coords))
-                        {
-                            var question = new IntegerValueQuest("MatrixQuestion",
-                                QuestionGenerator.MatrixMultQuestion(i, j), resultMatrix.Values[i][j]);
-                        }
-                    }
-                }
-            }
-
-            return resultMatrix;
-        }
-
-        public static List<Coordinate> GetRandomCoords(int rows, int columns)
-        {
-            var coords = new List<Coordinate>();
-            var random = new Random();
-
-            coords.Add(new Coordinate(1, 1));
-
-            for (var i = 1; i <= Math.Min(2, columns - 1); i++)
-            {
-                while (true)
-                {
-                    var newCoord = new Coordinate(1, 2 + random.Next() % (columns - 1));
-                    if (Coordinate.DoesCoordinateExist(newCoord, coords))
-                    {
-                        continue;
-                    }
-                    coords.Add(newCoord);
-                    break;
-                }
-            }
-
-            var cellsCount = (int) (rows * columns * 0.3);
-
-            for (var i = 1; i <= cellsCount; i++)
-            {
-                while (true)
-                {
-                    var newCoord = new Coordinate(2 + random.Next() % (rows - 1), 1 + random.Next() % columns);
-                    if (Coordinate.DoesCoordinateExist(newCoord, coords))
-                    {
-                        continue;
-                    }
-                    coords.Add(newCoord);
-                    break;
-                }
-            }
-
-            return coords;
         }
     }
 }
--- AlgoTeacher/MatrixMultiplyForm.cs
+++ AlgoTeacher/MatrixMultiplyForm.cs
@@ -7,13 +7,20 @@ using System.Text;
 using System.Linq;
 using System.Threading.Tasks;
 using System.Windows.Forms;
+using AlgoTeacher.Logic;
 using AlgoTeacher.User_Controls;
 using DevExpress.XtraEditors;
 
 namespace AlgoTeacher
 {
-    public partial class MatrixMultiplyForm : DevExpress.XtraEditors.XtraForm 
+    public partial class MatrixMultiplyForm : DevExpress.XtraEditors.XtraForm
     {
+        private QuestEvents.QuestEventHandler _questHandler;
+        private FillEvents.FillEventHandler _fillHandler;
+        private MatrixMultiply _matrixMultiply;
+        private Matrix _matrix1;
+        private Matrix _matrix2;
+
         public MatrixMultiplyForm()
         {
             InitializeComponent();
@@ -22,6 +29,15 @@ namespace AlgoTeacher
 
             QuestionControl.CalculateClickedHandler calculateClickHandler = new QuestionControl.CalculateClickedHandler(CalculateButton_Clicked);
             questionControl.CalculateClicked += calculateClickHandler;
+
+            _matrix1 = new Matrix(3, 3);
+            _matrix2 = new Matrix(3, 3);
+            _questHandler = new QuestEvents.QuestEventHandler(QuestEventHandler);
+            _fillHandler = new FillEvents.FillEventHandler(FillEventHandler);
+
+            MatrixMultiply matrixMult = new MatrixMultiply();
+            _matrixMultiply = matrixMult;
+            _matrixMultiply.questEvent += _questHandler;
         }
         private void MatrixMultiplyForm_Load(object sender, EventArgs e)
         {
@@ -31,6 +47,7 @@ namespace AlgoTeacher
         {
             // Действия при нажатии посчитать
             MessageBox.Show("Hi");
+            _matrixMultiply.MatrixMult(_matrix1, _matrix2);
         }
 
         public void AnswerButton_Clicked(object sender, EventArgs e)
@@ -41,6 +58,16 @@ namespace AlgoTeacher
         private void MatrixMultiplyForm_FormClosed(object sender, FormClosedEventArgs e)
         {
             this.Dispose();
+        }
+
+        public void QuestEventHandler(object sender, QuestEvents.QuestEventArgs e)
+        {
+            MessageBox.Show("Quest works");
+        }
+
+        public void FillEventHandler(object sender, FillEvents.FillEventArgs e)
+        {
+            MessageBox.Show("Fill works");
         }
     }
 }
